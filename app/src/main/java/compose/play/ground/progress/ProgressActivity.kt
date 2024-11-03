55de94ab86c27981f01f59be38dc64ef1cca524c
kangmin1012 <kangmin1012@naver.com>
2024-11-03 15:55:55 +0900

    [ProgressBar] Dash Progress 케이스 수정

--- app/src/main/java/compose/play/ground/progress/ProgressActivity.kt
+++ app/src/main/java/compose/play/ground/progress/ProgressActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.IntRange
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -18,16 +19,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import compose.play.ground.ui.theme.ComposePlayGroundTheme
 
@@ -49,61 +59,123 @@ class ProgressActivity : ComponentActivity() {
 
 @Composable
 private fun ProgressScreen() {
+    var progress by remember { mutableIntStateOf(0) }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
-            AnimateProgressBar()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(Color.White),
+        ) {
+            DashProgress(
+                progress = progress,
+                maxProgress = 10,
+                progressFillSpeedMillisecond = 100,
+                progressHeight = 10.dp
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            DashProgress(5, 10)
+
+            Button(onClick = {
+                progress = (0..10).filter { it != progress }.random()
+            }) {
+                Text("Randomize Progress")
+            }
         }
     }
 }
 
 /**
- * 높이가 20dp인 프로그레스 바를 애니메이션으로 증가시키는 UI Composable
+ * 대시 형태의 프로그레스바
+ *
+ * @param progress 진행 상태
+ * @param maxProgress 최대 프로그레스 진행도
+ * @param progressFillSpeedMillisecond 프로그레스 채워지는 속도
  */
 @Composable
-private fun AnimateProgressBar() {
-    var progress by remember { mutableFloatStateOf(0f) }
-    val animatedProgress by animateFloatAsState(
-        targetValue = progress,
+internal fun DashProgress(
+    @IntRange(from = 0, to = 10) progress: Int,
+    @IntRange(from = 1, to = 10) maxProgress: Int = 10,
+    progressFillSpeedMillisecond: Int = 100,
+    progressHeight: Dp = 6.dp,
+    modifier: Modifier = Modifier
+) {
+    var nowProgress by remember { mutableIntStateOf(progress) }
+    var previousProgress by remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(progress) {
+        previousProgress = nowProgress
+        nowProgress = progress
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(progressHeight),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            for (index in 1..maxProgress) {
+                DashItem(
+                    modifier = Modifier.weight(1f),
+                    isFilled = index <= nowProgress,
+                    speedMillis = progressFillSpeedMillisecond,
+                    delayMillis =
+                    if (nowProgress > previousProgress) {
+                        if (index in (previousProgress + 2)..progress) {
+                            progressFillSpeedMillisecond * (index - previousProgress - 1)
+                        } else {
+                            0
+                        }
+                    } else {
+                        if (index in (progress + 1)..previousProgress) {
+                            progressFillSpeedMillisecond * (previousProgress - index)
+                        } else {
+                            0
+                        }
+                    }
+
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DashItem(
+    modifier: Modifier = Modifier,
+    isFilled: Boolean,
+    speedMillis: Int = 0,
+    delayMillis: Int = 0,
+) {
+    val scaleAnimation by animateFloatAsState(
+        targetValue = if (isFilled) 1f else 0f,
         animationSpec = tween(
-            durationMillis = 2000,
-            easing = LinearOutSlowInEasing
+            durationMillis = speedMillis,
+            delayMillis = delayMillis,
+            easing = LinearEasing
         ),
-        label = "증가 애니메이션"
+        label = ""
     )
 
-    LaunchedEffect(Unit) {
-        progress = 1f
-    }
-
-    Box(modifier = Modifier.fillMaxWidth().height(20.dp).background(Color.LightGray)) {
-        Box(modifier = Modifier.height(20.dp).fillMaxWidth(animatedProgress).background(Color.Blue))
-    }
-}
-
-/**
- * Dash 형태로 구성되는 프로그레스 바를 표현하는 UI Composable
- *
- * @param progress
- * @param maxProgress
- */
-@Composable
-private fun DashProgress(@IntRange(from = 1, to = 10) progress: Int, @IntRange(from = 1, to = 10) maxProgress: Int) {
-    Row(
-        modifier = Modifier.fillMaxWidth().height(20.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+        color = Color(0xFFEDEDED)
     ) {
-        for (index in 1 .. maxProgress) {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(20.dp)
-                    .background(
-                        if (index <= progress) Color.Blue else Color.LightGray
-                    )
-            )
-        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    transformOrigin = TransformOrigin(0f, 0.5f)
+                    scaleX = scaleAnimation
+                }
+                .background(Color(0xFFFF7949))
+        )
     }
 }
 
@@ -112,22 +184,5 @@ private fun DashProgress(@IntRange(from = 1, to = 10) progress: Int, @IntRange(f
 private fun ProgressScreenPreview() {
     ComposePlayGroundTheme {
         ProgressScreen()
-    }
-}
-
-
-@Composable
-@Preview
-private fun AnimateProgressBarPreview() {
-    ComposePlayGroundTheme {
-        AnimateProgressBar()
-    }
-}
-
-@Composable
-@Preview(showBackground = true)
-private fun DashProgressPreview() {
-    ComposePlayGroundTheme {
-        DashProgress(progress = 5, maxProgress = 6)
     }
 }
